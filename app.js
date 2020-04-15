48760bef8860a8860da82d3d0cf2c9da0023d6a2
Rotem Goldshtein <rotemgoldshtein@gmail.com>
2020-04-15 19:53:11 +0300

    fix syntax

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
@@ -8,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-	res.render("./index");
+	res.render("index");
 });
 
 
@@ -21,11 +23,11 @@ app.get("/", function(req, res){
 
 
 
-app.listen(3000, function(){
-	console.log("server listen on port 3000");
-});
-
-
-// app.listen(process.env.PORT, process.env.IP, function(){
-// 	console.log("Server has started");
+// app.listen(3000, function(){
+// 	console.log("server listen on port 3000");
 // });
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("Server has started");
+});
