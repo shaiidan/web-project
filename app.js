51771a8c266403bcd3db1f64b24f92d1547c91b6
Rotemgold <44928659+Rotemgold@users.noreply.github.com>
2020-05-05 16:23:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.use(uploadUnit);
 
 
 
-app.listen(3000, function(){
-	console.log("server listen on port 3000");
-});
+ app.listen(process.env.PORT, process.env.IP, function(){
+ 	console.log("Server has started");
+ });
