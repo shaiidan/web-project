04a3911658ccaf0f6de9ba218ed07f911c1987b0
Rotemgold <44928659+Rotemgold@users.noreply.github.com>
2020-05-06 16:59:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -47,11 +47,11 @@ app.use(apartmentRouter);
 
 
 
- //app.listen(process.env.PORT, process.env.IP, function(){
- //	console.log("Server has started");
- //});
+ app.listen(process.env.PORT, process.env.IP, function(){
+ 	console.log("Server has started");
+ });
 
  
-app.listen(3000, function(){
-	console.log("server listen on port 3000");
-});
+// app.listen(3000, function(){
+// 	console.log("server listen on port 3000");
+// });
