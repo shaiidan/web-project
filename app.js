2f731046391bb0851bf4365c4de8ee59c4e677fb
Rotem Goldshtein <rotemgoldshtein@gmail.com>
2020-05-09 19:32:43 +0300

    update

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const dotenv = require('dotenv');
-const { Connection, Request } = require("tedious");
 const alert = require("alert-node");
 const indexRoutes  = require('./routes/index');
-const dbConfig = require ('./models/dbconfig');
 const registerOnwerRoutes = require('./routes/registerowner');
 const registerStudentRoutes = require('./routes/registerstudent');
 const termsRoutes = require('./routes/terms');
@@ -14,23 +12,15 @@ const contactUsRoutes = require('./routes/contactus');
 const studentHomePage = require('./routes/StudentHomepage');
 const ApartmentOwnerHomepage = require('./routes/ApartmentOwnerHomepage');
 const uploadUnit = require('./routes/uploadUnit');
-
+const recoveryPassword = require("./routes/recoveryPassword");
+const reset = require("./routes/reset");
+const upload = require("./routes/upload");
+const validate = require("./routes/valid");
 dotenv.config();
-config.options.trustServerCertificate = true;
-const connection = new Connection(dbConfig);
-connection.connect(); 
-// Attempt to connect and execute queries if connection goes through
-connection.on("connect", err => {
-	if (err) {
-	  console.error(err.message);
-	} else {
-	  console.log("DB connection success");
-	}
-});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(indexRoutes);
 app.use(registerOnwerRoutes);
 app.use(registerStudentRoutes);
@@ -40,8 +30,10 @@ app.use(contactUsRoutes);
 app.use(studentHomePage);
 app.use(ApartmentOwnerHomepage);
 app.use(uploadUnit);
-
-
+app.use(recoveryPassword);
+app.use(reset);
+app.use(upload);
+app.use(validate);
 
 app.listen(3000, function(){
 	console.log("server listen on port 3000");
