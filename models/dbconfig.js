463a4b290ee36c4eea8af4cd4df5589d9d216e17
Rotemgold <44928659+Rotemgold@users.noreply.github.com>
2020-05-11 20:02:17 +0300

    Update dbconfig.js

--- models/dbconfig.js
+++ models/dbconfig.js
@@ -1,16 +1,18 @@
+  
 module.exports = config = {
 	authentication: {
 	  options: {
-		userName: "samiroom", 
-		password: "Lucas2020" 
+		userName: process.env.AZURENAME, 
+		password: process.env.PASS
 	  },
 	  type: "default"
 	},
 	server: "samiroom.database.windows.net", 
 	options: {
 	  database: "samiroomDB",
-	  encrypt: true
+	  encrypt: true,
+	  enableArithAbort: true,
+	  rowCollectionOnRequestCompletion:true,
+	  trustServerCertificate: true,
 	}
   };
-  config.options.trustServerCertificate = true;
-
