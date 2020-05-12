11e87f51f822a8871aaeadad721f62597ef35b67
Rotemgold <44928659+Rotemgold@users.noreply.github.com>
2020-05-12 10:39:00 +0300

    Update registerstudent.js

--- routes/registerstudent.js
+++ routes/registerstudent.js
@@ -1,46 +1,44 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-use-before-define */
 const express = require ("express");
 const router = express.Router();
 const { Connection, Request } = require("tedious");
 const alert = require("alert-node");
-const registerUtils = require ('../models/registerUtils');
+const registerUtils = require('../models/registerUtils');
 
-
-router.get("/registerowner", function(req, res){
-	res.render("registerowner");
+router.get("/registerstudent", function(req, res){
+	res.render("registerstudent");
 });
 
-//registerowner post request:
-router.post("/registerowner", function(req, res){
+//registerstudent post request:
+router.post("/registerstudent", function(req, res, file){
 	const id = req.body.id;
 	const name = req.body.name;
+	const phone = req.body.phone;
 	const email = req.body.email;
 	const password = req.body.password;
-	const phone = req.body.phone;
+	const validation = 0;
 	const confirm = req.body.confirm;
 	if(confirm!=password){
-		alert("Password not match");
-		res.redirect("/registersowner");
+		alert("Password not match, try again");
 	}
 	else{
-    registerUtils.checkEmailAndId(email,id,function(result){ 
-		if(result == true){
-            registerUtils.addOwner(id, name, email, password, phone, function(result){
-                if(result == true){
-                    res.redirect("/ApartmentOwnerHomePage?id="+id+'&fullName='+full_name);
-                }else {
-                    console.log("register failed");
-                    res.redirect('/registerowner');
-                } 
-            });
+	registerUtils.checkEmailAndId(email,id,function(result){
+		if(result==true){
+      registerUtils.addStudent(email, id, phone, name, password, validation,function(result){
+        if(result == true){
+          res.redirect("/upload?email="+email);
         }
-        else {
-            console.log("login failed,Email or ID already exist");
-		    alert("Login failed,Email or ID already exist");
+        else{
+            console.log("somthingwrong");
+            res.redirect("/registerstudent");
         }
-	});
-}
+      });
+    }
+		else{
+			console.log("login failed,Email or ID already exist");
+			alert("Login failed,Email or ID already exist");
+    }
+});
+	}
 });
 
 module.exports = router;
