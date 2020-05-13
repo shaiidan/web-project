7a9fb6560d926ac962ec2cb0ea5d8255cd4d6364
Rotemgold <44928659+Rotemgold@users.noreply.github.com>
2020-05-13 12:28:38 +0300

    Update registerstudent.js

--- routes/registerstudent.js
+++ routes/registerstudent.js
@@ -1,22 +1,20 @@
 const express = require ("express");
 const router = express.Router();
 const { Connection, Request } = require("tedious");
-const alert = require("alert-node");
 const registerUtils = require('../models/registerUtils');
-
+const md5 = require('md5');
 router.get("/registerstudent", function(req, res){
 	res.render("registerstudent");
 });
-
 //registerstudent post request:
 router.post("/registerstudent", function(req, res, file){
 	const id = req.body.id;
 	const name = req.body.name;
 	const phone = req.body.phone;
 	const email = req.body.email;
-	const password = req.body.password;
+	const password = md5(req.body.password);
 	const validation = 0;
-	const confirm = req.body.confirm;
+	const confirm = md5(req.body.confirm);
 	if(confirm!=password){
 		res.render('registerstudent', {
 			msg: 'Password are not match, try again'
