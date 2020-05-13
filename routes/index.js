47305e109f38cb7354d951f3063e854762d2b289
Rotemgold <44928659+Rotemgold@users.noreply.github.com>
2020-05-13 12:38:30 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,6 @@ router.get("/", function(req, res){
 });
 
 
-router.get("/index", function(req, res){
-	res.render("index",);
-});
-
-
 //login post request:
 router.post("/index", function(req, res){
 	const email = req.body.email;
