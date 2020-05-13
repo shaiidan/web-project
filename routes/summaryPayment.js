3e0046d38da3f6b06715d347d804cb6c1067ae08
Rotemgold <44928659+Rotemgold@users.noreply.github.com>
2020-05-13 13:36:16 +0300

    Update summaryPayment.js

--- routes/summaryPayment.js
+++ routes/summaryPayment.js
@@ -2,48 +2,56 @@ const express = require ("express");
 const router = express.Router();
 const orders = require("../models/newOrders");
 const unit = require("../models/RentalHousingUnits");
+const authenticate = require("./authenticate").redirectHome;
 
 
-router.get("/summaryPayment", function(req, res){
+
+router.get("/summaryPayment",authenticate, function(req, res){
 	const order_id = req.query.orderId;
 	const user_id = req.query.id;
 	const full_name = req.query.fullName;
+	if(user_id===req.session.userId){
+		try{
+			orders.getOrder(order_id,function(result){
+				if(result != false){
+					res.render('summaryPayment',{order:result,id:user_id,fullName:full_name});
+					setTimeout(() => {
+						unit.changeStatusForOrder(result.unitID,1,function(){
+							if(result){
+								orders.deleteOrder(result.orderID,function(){
+									res.end();
+								});
+							}
+					});}, 900000);
+				}
+				else{
+					orders.getUnitIDfromOrderID(order_id,function(unitID){
+						unit.changeStatusForOrder(unitID,1,function()
+					{
+						orders.deleteOrder(order_id,function(){
+							res.redirect('/studentHomePage?id='+user_id+"&fullName="+full_name,404);});
+						});
+						})
+				
+					}
 	
-	try{
-		orders.getOrder(order_id,function(result){
-			if(result != false){
-				res.render('summaryPayment',{order:result,id:user_id,fullName:full_name});
-				setTimeout(() => {unit.changeStatusForOrder(result.unitID,1,function(){
-					if(result)
-					orders.deleteOrder(result.orderID,function(){
-					res.redirect('/studentHomePage?id='+user_id+"&fullName="+full_name,404);});
-				});
-					
-				}, 900000);
-			}
-			else{
-				orders.getUnitIDfromOrderID(order_id,function(unitID){
-					unit.changeStatusForOrder(unitID,1,function()
-				{
-					orders.deleteOrder(order_id,function(){
-						res.redirect('/studentHomePage?id='+user_id+"&fullName="+full_name,404);});
+			});
+		}
+		catch(e){
+			console.log("Error!!"+e);
+			orders.getUnitIDfromOrderID(order_id,function(unitID){
+			unit.changeStatusForOrder(unitID,1,function()
+					{
+						orders.deleteOrder(orderID,function(){
+							res.redirect('/studentHomePage?id='+user_id+"&fullName="+full_name,404);});
+						});
 					});
-					})
-			
-				}
-
-		});
+					}
 	}
-	catch(e){
-		console.log("Error!!"+e);
-		orders.getUnitIDfromOrderID(order_id,function(unitID){
-		unit.changeStatusForOrder(unitID,1,function()
-				{
-					orders.deleteOrder(orderID,function(){
-						res.redirect('/studentHomePage?id='+user_id+"&fullName="+full_name,404);});
-					});
-				});
-				}
+	else{
+        res.render("Error");
+    }
+	
 });
 
 
