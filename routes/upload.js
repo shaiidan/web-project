e5d7f5dc3ddc0fc45f60816fa2d403c2c0b74f12
shaiidan <shaiidan@hotmail.com>
2020-06-15 20:45:06 +0300

    updare circleCI cover

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const multer = require('multer');
-const ejs = require('ejs');
 const path = require('path');
 const router = express.Router();
 const app = express();
